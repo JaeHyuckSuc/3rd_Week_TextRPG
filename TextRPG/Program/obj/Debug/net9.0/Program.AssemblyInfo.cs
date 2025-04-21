37c9ac7c3fa5ed77e75ba46ed8e098b483c19862
Hicanos <ruda32168@gmail.com>
2025-04-21 16:36:35 +0900

    fix
    ignore 파일로 인한 변경점 있어서 커밋 추가함

--- TextRPG/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
+++ TextRPG/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d9a7bfd92f1698c4cb15fc283a4bc18a42e21e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4710b131fb014db9c4a91d10de55c16555d1de3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
